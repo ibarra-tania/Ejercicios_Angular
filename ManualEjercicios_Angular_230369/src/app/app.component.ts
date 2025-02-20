1d74cbdc027a177dab74a02389154eb62ed73534
Tania Ibarra Salgado <230369@utxicotepec.edu.mx>
2025-02-19 23:43:39 -0600

    Angular-Ejercicios
    Ingreso de ejercicios en Angular para introducirse al tema

---
+++ ManualEjercicios_Angular_230369/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { Ejercicio01Component } from './ejercicio-01/ejercicio-01.component';
+import { Ejercicio02Component } from './ejercicio-02/ejercicio-02.component';
+import { Ejercicio03Component } from './ejercicio-03/ejercicio-03.component';
+import { Ejercicio04Component } from './ejercicio-04/ejercicio-04.component';
+import { Ejercicio05Component } from './ejercicio-05/ejercicio-05.component';
+import { Ejercicio06Component } from './ejercicio-06/ejercicio-06.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true, 
+  imports: [RouterOutlet, Ejercicio01Component, Ejercicio02Component, Ejercicio03Component, Ejercicio04Component, Ejercicio05Component, Ejercicio06Component],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ManualEjercicios_Angular_230369';
+}
---
+++ ManualEjercicios_Angular_230369/src/app/ejercicio-01/ejercicio-01.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio-01',
+  imports: [],
+  template: `
+    Hello Universe
+  `,
+  styles: `
+    :host {
+      color: #a144eb;
+    }
+  `
+  })
+export class Ejercicio01Component {
+
+}
