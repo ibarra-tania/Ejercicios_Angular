4aafd8734b2770584ec68017e5d0901bc0e0fa3a
Tania Ibarra Salgado <230369@utxicotepec.edu.mx>
2025-03-03 16:24:06 -0600

    angular
    Este

--- ManualEjercicios_Angular_230369/src/app/app.component.ts
+++ ManualEjercicios_Angular_230369/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { AuthService } from './services/auth.service';
+
 import { Ejercicio01Component } from './ejercicio-01/ejercicio-01.component';
 import { Ejercicio02Component } from './ejercicio-02/ejercicio-02.component';
 import { Ejercicio03Component } from './ejercicio-03/ejercicio-03.component';
@@ -18,13 +20,38 @@ import { PageContentComponent } from './components/page-content/page-content.com
 import { FooterContentComponent } from './components/footer-content/footer-content.component';
 import { BreadconmbsComponent } from './components/breadconmbs/breadconmbs.component';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet, Ejercicio01Component, Ejercicio02Component, Ejercicio03Component, Ejercicio04Component, Ejercicio05Component, Ejercicio06Component, Ejercicio07Component ,MatButtonModule, ToolbarComponent, NavbarComponent, SidebarComponent, PageContentComponent, FooterContentComponent, BreadconmbsComponent],
+  imports: [RouterOutlet, Ejercicio01Component, CommonModule, FormsModule, Ejercicio02Component, Ejercicio03Component, Ejercicio04Component, Ejercicio05Component, Ejercicio06Component, Ejercicio07Component ,MatButtonModule, ToolbarComponent, NavbarComponent, SidebarComponent, PageContentComponent, FooterContentComponent, BreadconmbsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ManualEjercicios_Angular_230369_TIS'; 
+  isAuthenticated = true;
+  title = 'ManualEjercicios_Angular_230369_TIS';
+  
+  isSidebarOpen = true;
+  
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.isAuthenticated.subscribe(status => {
+      this.isAuthenticated = status;
+    });
+  }
+
+  // Método para iniciar sesión
+  login() {
+    this.authService.login(); // Cambia el estado de autenticación a true
+  }
+
+
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
--- ManualEjercicios_Angular_230369/src/app/components/page-content/page-content.component.ts
+++ ManualEjercicios_Angular_230369/src/app/components/page-content/page-content.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { FooterContentComponent } from '../footer-content/footer-content.component';
-import { BreadconmbsComponent } from '../breadconmbs/breadconmbs.component';
 
 @Component({
   selector: 'app-page-content',
-  imports: [FooterContentComponent, BreadconmbsComponent],
+  imports: [],
   templateUrl: './page-content.component.html',
   styleUrl: './page-content.component.css'
 })
